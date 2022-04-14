78ee01b916ab1efa148efc9cb4d573d769b173cf
Арбузов Вадим <www.vad.a@mail.ru>
2022-04-14 16:48:11 +0300

    changed ref

--- ConsoleSnakeTest/Properties/AssemblyInfo.cs
+++ ConsoleSnakeTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleSnakeTest")]
+[assembly: AssemblyTitle("ConsoleSnake")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleSnakeTest")]
+[assembly: AssemblyProduct("ConsoleSnake")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
